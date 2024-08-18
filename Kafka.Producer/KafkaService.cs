71ca0c46a4243b6e492a9e0377412d3d52c88cfe
ilhan enes daniş <danisenes3@gmail.com>
2024-08-18 15:24:42 +0300

    Add project files.

---
+++ Kafka.Producer/KafkaService.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka.Admin;
+using Confluent.Kafka;
+
+namespace Kafka.Producer
+{
+    public class KafkaService
+    {
+        public async Task CrateTopic(string topicName)
+        {
+
+            using var adminClient = new AdminClientBuilder(new AdminClientConfig()
+            {
+                BootstrapServers = "localhost:9094"
+            }).Build();
+
+            try
+            {
+                await adminClient.CreateTopicsAsync(new[]
+                {
+                   new TopicSpecification(){Name=topicName,NumPartitions=3,ReplicationFactor=1}
+ 
+                });
+                Console.WriteLine($"{topicName} is created");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+
+
+        }
+    }
+}
